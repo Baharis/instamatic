9026a5b46b966b7ad1776bb9a308c7665eb94270
Daniel Tchoń <dantch@onet.eu>
2026-01-20 19:31:06 +0100

    Mostly finished scaling and formalized multiprocessing logic.

--- src/instamatic/experiments/sped/diffhunt.py
+++ src/instamatic/experiments/sped/diffhunt.py
@@ -3,100 +3,244 @@ from __future__ import annotations
 import multiprocessing as mp
 import multiprocessing.shared_memory
 import queue
+import threading
 import uuid
+from dataclasses import dataclass, field
+from typing import Optional
 
 import numpy as np
+import pandas as pd
+from typing_extensions import Literal
 
-from instamatic.experiments.sped.util import SPEDLoc
+from instamatic._typing import AnyPath
 
 N_PROCESSORS = 4
 
 
 mp.set_start_method('spawn', force=True)
 
+Task = Literal['PROCESS', 'WRITE', 'TERMINATE']
+Event = Literal['PROCESSING', 'PROCESSED', 'SWITCHED', 'TERMINATED']
+
+
+@dataclass(frozen=True)
+class Command:
+    """Schema used to communicate commands from dispatcher to any worker."""
+
+    task: Task
+    buffer_name: Optional[str] = None
+    buffer_pointer: Optional[int] = None
+    kwargs: Optional[dict] = None
+
+
+@dataclass(frozen=True)
+class Feedback:
+    """Schema used to communicate feedback from any worker to dispatcher."""
+
+    event: Event
+    worker_id: int
+    buffer_name: Optional[str] = None
+    buffer_pointer: Optional[int] = None
+    details: Optional[dict] = None
+
 
 class DiffHuntDispatcher:
     """Proxy class: ask workers on other processes if image has diffraction"""
+
+    @dataclass
+    class Worker:
+        buffer: str = ''
+        busy: bool = False
+        pointer: Optional[int] = None
+        process: mp.Process = None
+
+    @dataclass
+    class Buffer:
+        frames: np.ndarray
+        name: str = field(default_factory=lambda: uuid.uuid4().hex)
+        pointer: int = 0
+        pointers: set[int] = field(default_factory=set)  # currently processed
+        workers: set[int] = field(default_factory=set)  # attached to buffer
 
     def __init__(self, shape, dtype):
         self.shape: tuple[int, int] = shape
         self.dtype: np.dtype = dtype
 
-        self.buffer: np.ndarray = np.array([], dtype=dtype)
-        self.buffer_name: str = ''
-        self.buffer_ptr: int = 0
+        self.commands: mp.Queue[Command] = mp.Queue()
+        self.feedback: mp.Queue[Feedback] = mp.Queue()
+        self.workers: dict[int, DiffHuntDispatcher.Worker] = self.initialize_workers()
+        self.buffers: dict[str, DiffHuntDispatcher.Buffer] = {}
+        self.history = pd.DataFrame(columns=['buffer', 'pointer', 'has_diffraction', 'header'])
+        self.history.set_index(['buffer', 'pointer'], inplace=True)
 
-        self.queries = mp.Queue()
-        self.answers = mp.Queue()
-        self.workers: list[mp.Process] = self.initialize_workers()
-
-    def initialize_workers(self) -> list[mp.Process]:
+    def initialize_workers(self) -> dict[int, DiffHuntDispatcher.Worker]:
         """Run once at the start of experiment to spawn eval processes."""
-        args = (self.queries, self.answers, self.dtype)
-        workers = []
+        workers = {}
         for i in range(N_PROCESSORS):
-            p = mp.Process(target=diff_hunt_worker, args=(i, *args), daemon=1)
-            workers.append(p)
-            p.start()
+            worker = DiffHuntWorker(i, self.commands, self.feedback, self.dtype)
+            worker.start()
+            workers[i] = self.Worker(process=worker)
         return workers
 
     def switch_buffer(self, n_frames: int = 100, name: str = None) -> None:
         """Configure a new mp shared memory space to buffer a frame stack."""
-        self.buffer_name = name if name is not None else 'SPED_' + uuid.uuid4().hex
-        buffer_shape = (n_frames, self.shape[0], self.shape[1])
-        size = np.prod(buffer_shape) * np.dtype(self.dtype).itemsize
-        shm = mp.shared_memory.SharedMemory(name=self.buffer_name, create=True, size=size)
-        self.buffer = np.ndarray(buffer_shape, dtype=self.dtype, buffer=shm.buf)
-        self.buffer_ptr: int = 0
+        shape = (n_frames, self.shape[0], self.shape[1])
+        size = np.prod(shape) * np.dtype(self.dtype).itemsize
+        shm = mp.shared_memory.SharedMemory(name=name, create=True, size=size)
+        frames = np.ndarray(shape, dtype=self.dtype, buffer=shm.buf)
+        self.buffers[name] = self.Buffer(frames=frames, name=name)
 
-    def submit(self, frame: np.ndarray) -> None:
-        """Request eval of 1 frame from buffer stored on some shared memory."""
-        if self.buffer_ptr >= self.buffer.shape[0]:
-            raise RuntimeError(f'{self.buffer_name} buffer overflow')
-        self.buffer[self.buffer_ptr, :, :] = frame
-        self.queries.put((self.buffer_name, self.buffer.shape, self.buffer_ptr))
-        self.buffer_ptr += 1
+    def write_buffer(self, path: AnyPath) -> None:
+        """Save all the frames with diffraction in an active buffer."""
+        ab = list(self.buffers.values())[-1]  # last i.e. active buffer
+        h = self.history
+        to_save = h[(h['buffer'] == ab.name) & h['has_diffraction']]
+        for ptr, h in to_save[['pointer', 'header']]:
+            self.emit('WRITE', ab.name, ptr, kwargs={'path': path, 'header': h})
 
-    def poll(self):
-        try:
-            return self.answers.get_nowait()
-        except queue.Empty:
-            return None
+    # COMMANDING METHODS THAT DISPATCH COMMANDS TO WORKERS
 
-    def close(self):
-        self.queries.put(None)
-        for p in self.workers:
-            p.join()
+    def emit(self, task: Task, *args, **kwargs) -> None:
+        """Shorthand to create and put Command in the self.commands queue."""
+        self.commands.put(Command(task, *args, **kwargs))
+
+    def process(self, frame: np.ndarray, header: Optional[dict]) -> None:
+        """Request 'PROCESS_FRAME' from buffer stored on some shared memory."""
+        ab = list(self.buffers.values())[-1]  # last i.e. active buffer
+        if ab.pointer >= ab.frames.shape[0]:
+            raise RuntimeError(f'{ab.name} buffer overflow')
+        ab.frames[ab.pointer, :, :] = frame
+        self.emit('PROCESS', buffer_name=ab.name, buffer_pointer=ab.pointer)
+        ab.pointers.add(ab.pointer)
+        ab.pointer += 1
+        self.history.loc[(ab.name, ab.pointer), 'header'] = header
+        self.history.loc[(ab.name, ab.pointer), 'has_diffraction'] = None
+
+    def terminate_workers(self) -> None:
+        """Command all workers to 'TERMINATE' and report the success."""
+        for _ in self.workers:
+            self.emit('TERMINATE')
+
+    # HANDLE FEEDBACK INCOMING FROM THE WORKERS
+
+    def handle_feedback(self, stop_event: threading.Event) -> None:
+        """To be called in a separate thread to handle incoming feedback."""
+        while not stop_event.is_set():
+            try:
+                fb: Feedback = self.feedback.get(timeout=0.05)
+            except queue.Empty:
+                continue
+
+            worker = self.workers.get(fb.worker_id)
+
+            if fb.event == 'PROCESSING':
+                worker.busy = True
+                worker.buffer = fb.buffer_name
+                worker.pointer = fb.buffer_pointer
+                buffer = self.buffers.get(fb.buffer_name)
+                buffer.workers.add(fb.worker_id)
+                buffer.pointers.add(fb.buffer_pointer)
+
+            elif fb.event == 'PROCESSED_FRAME':
+                idx = (fb.buffer_name, fb.buffer_pointer)
+                has = fb.details.get('has_diffraction', 'False')
+                self.history.at[idx, 'has_diffraction'] = has
+                worker.busy = False
+                worker.pointer = None
+                buffer = self.buffers.get(fb.buffer_name)
+                buffer.pointers.discard(fb.buffer_pointer)
+
+            elif fb.event == 'SWITCHED':
+                if old_buffer := worker.buffer:
+                    self.buffers.get(old_buffer).workers.discard(fb.worker_id)
+                worker.buffer = fb.buffer_name
+                self.buffers.get(fb.buffer_name).workers.add(fb.worker_id)
+                self._maybe_release_buffer(self.buffers.get(fb.buffer_name))
+
+            elif fb.event == 'TERMINATE':
+                self._terminate_worker(fb.worker_id)
+
+            else:
+                raise ValueError(f'Unknown feedback event {fb.event}')
+
+    def _maybe_release_buffer(self, buffer: DiffHuntDispatcher.Buffer):
+        """If the buffer has no workers and no plans, release its memory."""
+        if not buffer.workers and not buffer.pointers:
+            del self.buffers[buffer.name]
+            try:
+                buffer.frames.base.close()
+                buffer.frames.base.unlink()
+            except Exception as e:
+                print(f'Warning: could not release buffer {buffer.name}: {e}')
+
+    def _terminate_worker(self, worker_id: int) -> None:
+        """Once the worker is ready to terminate, join and close it."""
+        worker = self.workers.pop(worker_id)
+        if worker.buffer:
+            buffer = self.buffers.get(worker.buffer)
+            if buffer:
+                buffer.workers.discard(worker_id)
+                self._maybe_release_buffer(buffer)
+        worker.process.join()
+        worker.process.close()
 
 
-def diff_hunt_worker(
-    worker_id: int,
-    queries: mp.Queue,
-    answers: mp.Queue,
-    dtype: np.dtype,
-):
-    """Evaluates if shared frames have diffraction on a separate processor."""
-    buffer: np.ndarray = np.array([], dtype=dtype)
-    buffer_name: str = ''
+class DiffHuntWorker(mp.Process):
+    """Stateful diffraction-hunting work process handled by the dispatcher."""
 
-    while True:
-        q = queries.get(block=True)
+    def __init__(
+        self,
+        worker_id: int,
+        commands: mp.Queue,
+        feedback: mp.Queue,
+        dtype: np.dtype,
+    ):
+        super().__init__(daemon=True)
+        self.worker_id = worker_id
+        self.commands = commands
+        self.feedback = feedback
+        self.dtype = dtype
 
-        if q is None:
-            queries.put(None)
-            return
+        self.buffer: np.ndarray = np.array([], dtype=dtype)
+        self.buffer_name: str = ''
 
-        assert isinstance(q, tuple) and len(q) == 3
-        q_buffer_name, q_buffer_shape, q_buffer_ptr = q
+    def emit(self, event: Event, *args, **kwargs) -> None:
+        """Put worker_id followed by all args in the self.feedback queue."""
+        self.feedback.put(Feedback(event, self.worker_id, *args, **kwargs))
 
-        if q_buffer_name != buffer_name:
-            buffer_name = q_buffer_name
+    def run(self) -> None:
+        """Main loop passing incoming commands to respective methods."""
+        while True:
+            cmd, *args = self.commands.get(block=True)
+
+            if cmd == 'TERMINATE':
+                self.emit('TERMINATED')
+                return
+
+            if cmd == 'PROCESS_FRAME':
+                self._process_frame(*args)
+                continue
+
+            raise ValueError(f'Unknown command: {cmd}')
+
+    def _process_frame(
+        self,
+        buffer_name: str,
+        buffer_shape: tuple,
+        frame_index: int,
+    ) -> None:
+        """Handles the 'PROCESS FRAME' command."""
+
+        if buffer_name != self.buffer_name:
+            self.buffer_name = buffer_name
             shm = mp.shared_memory.SharedMemory(name=buffer_name)
-            buffer = np.ndarray(q_buffer_shape, dtype=dtype, buffer=shm.buf)
+            self.buffer = np.ndarray(buffer_shape, dtype=self.dtype, buffer=shm.buf)
+            self.emit('SWITCHED', buffer_name=buffer_name)
 
-        frame = buffer[q_buffer_ptr]
-        has_diffraction: bool = detect_diffraction(frame)
-        answers.put((q_buffer_ptr, has_diffraction))
+        self.emit('PROCESSING', buffer_name=buffer_name, buffer_pointer=frame_index)
+        frame = self.buffer[frame_index]
+        d = {'has_diffraction': detect_diffraction(frame)}
+        self.emit('PROCESSED', buffer_name=buffer_name, buffer_pointer=frame_index, details=d)
 
 
 def detect_diffraction(frame: np.ndarray) -> bool:
